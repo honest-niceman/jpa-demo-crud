668bf020fe5cd627ee594b7494bd587477f547bf
Georgii Vlasov <honest.niceman@gmail.com>
2022-04-24 17:35:20 +0400

    Entities have been added

---
+++ src/main/java/io/jpabuddy/spring/demo/jpademo/entities/User.java
@@ -0,0 +1,69 @@
+package io.jpabuddy.spring.demo.jpademo.entities;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    @OneToMany(mappedBy = "manager")
+    private Set<Project> projects = new LinkedHashSet<>();
+
+    @Email(message = "Incorrect email")
+    @Column(name = "email", unique = true)
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(Set<Project> projects) {
+        this.projects = projects;
+    }
+
+}

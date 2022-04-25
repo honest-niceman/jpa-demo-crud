3fda2de9d5afb9295d585097d9364c0cd5baedfe
Georgii Vlasov <honest.niceman@gmail.com>
2022-04-25 11:24:26 +0400

    Task

--- src/main/java/io/jpabuddy/spring/demo/jpademo/entities/Task.java
+++ src/main/java/io/jpabuddy/spring/demo/jpademo/entities/Task.java
@@ -4,18 +4,21 @@ import javax.persistence.*;
 import java.time.Instant;
 
 @Entity
-@Table(name = "task")
+@Table(name = "task", indexes = {
+        @Index(name = "uc_task_name_project_id", columnList = "name, project_id", unique = true),
+        @Index(name = "idx_task_name", columnList = "name")
+})
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "project_id")
     private Project project;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "assignee_id")
     private User assignee;
 
@@ -27,6 +30,22 @@ public class Task {
 
     @Column(name = "name")
     private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
 
     public User getAssignee() {
         return assignee;
@@ -58,22 +77,6 @@ public class Task {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public Project getProject() {
-        return project;
-    }
-
-    public void setProject(Project project) {
-        this.project = project;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
 }
